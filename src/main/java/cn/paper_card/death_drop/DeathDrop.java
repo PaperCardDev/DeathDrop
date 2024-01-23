2443410b6542a74caf53a1eb06e16bb3360e5b7e
Paper99 <1398872296@qq.com>
2024-01-23 11:12:26 +0800

    显示凋落物的中文名和数量。

--- src/main/java/cn/paper_card/death_drop/DeathDrop.java
+++ src/main/java/cn/paper_card/death_drop/DeathDrop.java
@@ -1,6 +1,8 @@
 package cn.paper_card.death_drop;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.event.HoverEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
@@ -18,6 +20,42 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Random;
 
 public final class DeathDrop extends JavaPlugin implements Listener {
+
+    private final static String LINK = "https://pan90.gitee.io/docs/player-tips.html";
+
+    private void appendPrefix(@NotNull TextComponent.Builder text) {
+        text.append(Component.text("[").color(NamedTextColor.GRAY));
+        text.append(Component.text(this.getName()).color(NamedTextColor.DARK_AQUA));
+        text.append(Component.text("]").color(NamedTextColor.GRAY));
+    }
+
+    private @NotNull TextComponent suffix() {
+        return Component.text("[???]").color(NamedTextColor.GRAY).decorate(TextDecoration.UNDERLINED)
+                .hoverEvent(HoverEvent.showText(Component.text("点击查看说明")))
+                .clickEvent(ClickEvent.openUrl(LINK));
+    }
+
+    private void item(@NotNull TextComponent.Builder text, @NotNull ItemStack itemStack) {
+        final Material type = itemStack.getType();
+
+        final String itemTranslationKey = type.getItemTranslationKey();
+
+        text.append(Component.text("[").color(NamedTextColor.GRAY));
+
+        if (itemTranslationKey != null) {
+            text.append(Component.translatable(itemTranslationKey).color(NamedTextColor.GOLD).decorate(TextDecoration.BOLD));
+        } else {
+            text.append(Component.text(type.key().value())
+                    .hoverEvent(HoverEvent.showItem(type.getKey(), itemStack.getAmount()))
+                    .color(NamedTextColor.GOLD)
+                    .decorate(TextDecoration.UNDERLINED)
+                    .decorate(TextDecoration.BOLD)
+            );
+        }
+        text.append(Component.text("x").color(NamedTextColor.GRAY));
+        text.append(Component.text(itemStack.getAmount()).color(NamedTextColor.DARK_AQUA));
+        text.append(Component.text("]").color(NamedTextColor.GRAY));
+    }
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void on(@NotNull PlayerDeathEvent event) {
@@ -62,21 +100,24 @@ public final class DeathDrop extends JavaPlugin implements Listener {
         if (itemStack != null) {
             event.getDrops().add(itemStack);
 
-            final Material type = itemStack.getType();
+            final TextComponent.Builder text = Component.text();
+            this.appendPrefix(text);
+            text.appendSpace();
+            text.append(Component.text("本次死亡掉落的物品为：").color(NamedTextColor.RED));
+            this.item(text, itemStack);
+            text.appendSpace();
+            text.append(this.suffix());
 
-            player.sendMessage(Component.text()
-                    .append(Component.text("本次死亡掉落的物品为：").color(NamedTextColor.RED))
-                    .append(Component.text(type.key().value())
-                            .hoverEvent(HoverEvent.showItem(type.getKey(), itemStack.getAmount()))
-                            .color(NamedTextColor.GOLD)
-                            .decorate(TextDecoration.BOLD)
-                    )
-                    .build());
-
+            player.sendMessage(text.build());
         } else {
-            player.sendMessage(Component.text("很幸运，本次死亡没有掉落任何物品~").color(NamedTextColor.GREEN));
+            final TextComponent.Builder text = Component.text();
+            this.appendPrefix(text);
+            text.appendSpace();
+            text.append(Component.text("很幸运，本次死亡没掉落任何物品~").color(NamedTextColor.GREEN));
+            text.appendSpace();
+            text.append(this.suffix());
+            player.sendMessage(text.build());
         }
-
     }
 
     @Override
